2d4ca6bc3574894eb9c1950789179af29d5d0122
Austin Warrick <austinwarrick@gmail.com>
2023-08-25 10:33:19 -0500

    changed file names to singular

--- rareapi/models/__init__.py
+++ rareapi/models/__init__.py
@@ -1,5 +1,5 @@
 from .reaction import Reaction
-from .postreactions import PostReaction
+from .postreaction import PostReaction
 from .tags import Tag
-from .post_tags import PostTags
+from .post_tag import PostTags
 from .comment import Comment
