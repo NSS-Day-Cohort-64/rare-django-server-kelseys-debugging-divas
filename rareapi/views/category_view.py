52b9f472e3a6c2025de40e317346f1ed938263a0
Austin Warrick <austinwarrick@gmail.com>
2023-08-31 14:03:35 -0500

    gotta do it

--- rareapi/views/category_view.py
+++ rareapi/views/category_view.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from rareapi.models import Category
+\
 
 
 class CategoryView(ViewSet):
